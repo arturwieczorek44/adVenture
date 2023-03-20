0068939b372090220828261839b6a671fe95f129
atlas45-arc <arturwieczorek45@gmail.com>
2023-03-20 18:35:33 +0100

    Initial commit

---
+++ src/main/java/adventure/adventure/AdVentureApplication.java
@@ -0,0 +1,13 @@
+package adventure.adventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdVentureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdVentureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/adventure/adventure/AdVentureApplicationTests.java
@@ -0,0 +1,13 @@
+package adventure.adventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdVentureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

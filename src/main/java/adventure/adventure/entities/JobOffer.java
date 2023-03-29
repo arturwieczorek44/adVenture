a27ddde25721c43a7459f5eda8295882039013e8
Jakub <dzejkobinho@gmail.com>
2023-03-29 20:08:31 +0200

    Dodałem klasę encji Job Offer

---
+++ src/main/java/adventure/adventure/entities/JobOffer.java
@@ -0,0 +1,47 @@
+package adventure.adventure.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="JobOffer")
+public class JobOffer {
+    @Id
+    @Setter
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Getter @Setter
+    private String nazwa_stanowiska;
+    @Getter @Setter
+    private String opis;
+    @Getter @Setter
+    private String wymagania;
+    @Getter @Setter
+    private String lokalizacja;
+    @Getter @Setter
+    private double pensja;
+    @Getter @Setter
+    private Date data_skladania;
+
+
+    @Override
+    public String toString() {
+        return "JobOffer{" +
+                "id=" + id +
+                ", nazwa_stanowiska='" + nazwa_stanowiska + '\'' +
+                ", opis='" + opis + '\'' +
+                ", wymagania='" + wymagania + '\'' +
+                ", lokalizacja='" + lokalizacja + '\'' +
+                ", pensja=" + pensja +
+                ", data_skladania=" + data_skladania +
+                '}';
+    }
+}

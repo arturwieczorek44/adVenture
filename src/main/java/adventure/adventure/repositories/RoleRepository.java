bff54bbcdb4d13d4d5fc78e51951fad8b53854c8
kamilwalczynski <kamil.walczak000@gmail.com>
2023-04-14 16:30:18 +0200

    Utworzyłem klasę oraz kontroler dla ról użytkowników systemu

---
+++ src/main/java/adventure/adventure/repositories/RoleRepository.java
@@ -0,0 +1,8 @@
+package adventure.adventure.repositories;
+
+import adventure.adventure.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository <Role, Long> {
+    Role findByName(String name);
+}

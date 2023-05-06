e7622ec2ef9934567119a71236e8d1effd4d1f21
atlas45-arc <arturwieczorek45@gmail.com>
2023-05-06 20:30:48 +0200

    Working security but problem with db

--- src/main/java/adventure/adventure/repositories/EmploymentRepository.java
+++ src/main/java/adventure/adventure/repositories/EmploymentRepository.java
@@ -2,7 +2,9 @@ package adventure.adventure.repositories;
 
 import adventure.adventure.entities.Employment;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmploymentRepository extends CrudRepository<Employment, Long> {
 
 }

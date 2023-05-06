21bdb6e9d6439d58300a79568b0766566aa5d980
Jakub <dzejkobinho@gmail.com>
2023-05-06 22:52:45 +0200

    Poprawki interfejsu i kontrolera

--- src/main/java/adventure/adventure/controllers/LoginController.java
+++ src/main/java/adventure/adventure/controllers/LoginController.java
@@ -1,19 +1,23 @@
 package adventure.adventure.controllers;
 
+import adventure.adventure.entities.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/login_form")
 public class LoginController {
 
-    @GetMapping
+    @GetMapping()
     public String showLoginForm()
     {
-        return "loginform";
+        return "login_form";
     }
 
-    @PostMapping
-    public String login(@RequestParam String username, @RequestParam String password) {
-        return "/";
+    @PostMapping()
+    public String login(User user, Model model) {
+        model.addAttribute("message", "Udało ci się zalogować " + user.getUsername());
+        return "login_form";
     }
 }
